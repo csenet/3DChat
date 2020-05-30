bd8ef31ef94c87425a35b241c5cb3fc5be8cde31
kouic <kouichi.hirachi@gmail.com>
2020-05-30 13:27:24 +0900

    構文エラー修正

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,11 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        3DChat
-      </h1>
-      <h2 class="subtitle">
-        Web Chat system using 3D-avatar
-      </h2>
+      <h1 class="title">3DChat</h1>
+      <h2 class="subtitle">Web Chat system using 3D-avatar</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </div>
@@ -30,8 +18,8 @@ import Logo from '~/components/Logo.vue'
 
 export default Vue.extend({
   components: {
-    Logo
-  }
+    Logo,
+  },
 })
 </script>
 
@@ -46,8 +34,8 @@ export default Vue.extend({
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
